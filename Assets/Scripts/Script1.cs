39c81f51da043122814b1e39fa800767b32edd8b
VuManhCR7 <vumanh358@gmail.com>
2022-09-20 22:56:20 +0700

    change

--- Assets/Scripts/Script1.cs
+++ Assets/Scripts/Script1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VuMinhManh : MonoBehaviour
+public class Script1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
